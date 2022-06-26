93f327d77ce9eb5ac3d8e04058181cafd5b99427
gallardoS <swamikaas@gmail.com>
2022-06-26 22:40:36 +0200

    log pings

--- src/main/java/com/swamigallardo/mandalorian/PingController.java
+++ src/main/java/com/swamigallardo/mandalorian/PingController.java
@@ -1,5 +1,6 @@
 package com.swamigallardo.mandalorian;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class PingController {
 
     @Value("${mandalorian.ping.up}")
@@ -14,6 +16,7 @@ public class PingController {
 
     @GetMapping
     public String ping(){
+        log.info("Ping received");
         return ping;
     }
 }
